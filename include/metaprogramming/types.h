6b476ccbd2ae44e7a37a753cab7689d0201f13f5
liangti <liangti@foxmail.com>
2020-12-25 18:56:31 -0500

    Add decay types

--- include/metaprogramming/types.h
+++ include/metaprogramming/types.h
@@ -5,11 +5,112 @@
 
 namespace sql{
 
+// same_t check types equivalent
 template<typename T, typename U>
 struct same_t: std::false_type{};
 
 template<typename T>
 struct same_t<T, T>: std::true_type{};
+
+// remove_cv extract type from const volatile
+
+template<typename T>
+struct remove_cv{
+    using type = T;
+};
+
+template<typename T>
+struct remove_cv<const T>{
+    using type = T;
+};
+
+template<typename T>
+struct remove_cv<volatile T>{
+    using type = T;
+};
+template<typename T>
+struct remove_cv<const volatile T>{
+    using type = T;
+};
+
+// conditional <bool, T, U> gives T if bool=True, else U
+template<bool, typename T, typename F>
+struct conditional{
+    using type = T;
+};
+
+template<typename T, typename F>
+struct conditional<false, T, F>{
+    using type = F;
+};
+
+// remove_reference <T> remove & && from a type
+template<typename T>
+struct remove_reference{
+    using type = T;
+};
+
+template<typename T>
+struct remove_reference<T&>{
+    using type = T;
+};
+
+template<typename T>
+struct remove_reference<T&&>{
+    using type = T;
+};
+
+// remove_array <T> remove [] from a type
+template<typename T>
+struct remove_array{
+    using type = T;
+};
+
+template<typename T>
+struct remove_array<T[]>{
+    using type = T;
+};
+
+template<typename T, int N>
+struct remove_array<T[N]>{
+    using type = T;
+};
+
+// is_array determine if a type is an array
+template<typename T>
+struct is_array: std::false_type{};
+
+template<typename T>
+struct is_array<T[]>: std::true_type{};
+
+template<typename T, int N>
+struct is_array<T[N]>: std::true_type{};
+
+// is_function determine if a type is a function, TODO see cppreference for full support
+template<typename T>
+struct is_function: std::false_type{};
+
+template<typename Ret, typename... Args>
+struct is_function<Ret(Args...)>: std::true_type{};
+
+// ...
+
+// decay <T> remove array, reference, pointer, cv from a type
+template<typename T>
+struct decay{
+private:
+    using U = typename remove_reference<T>::type;
+public:
+    using type = typename conditional<
+        is_array<U>::value,
+        typename remove_array<U>::type*,
+        typename conditional<
+            std::is_function<U>::value,
+            typename std::add_pointer<U>::type,
+            typename remove_cv<U>::type
+        >::type
+    >::type;
+};
 
 }; // namespace sql
 
--- t/06-types.t.cpp
+++ t/06-types.t.cpp
@@ -3,13 +3,97 @@
 
 using namespace sql;
 
-TEST(test_tuple, construct_empty_tuple){
+TEST(test_types, check_type_equivalent){
     bool t1 = same_t<int, int>::value;
-    bool t2 = same_t<int, char>::value;
+    bool t2 = same_t<int, double>::value;
     bool t3 = same_t<int, int*>::value;
     bool t4 = same_t<int, int&>::value;
+    bool t5 = same_t<int, int&&>::value;
+    bool t6 = same_t<int, const int>::value;
+    bool t7 = same_t<int, volatile int>::value;
     EXPECT_TRUE(t1);
     EXPECT_FALSE(t2);
     EXPECT_FALSE(t3);
     EXPECT_FALSE(t4);
+    EXPECT_FALSE(t5);
+    EXPECT_FALSE(t6);
+    EXPECT_FALSE(t7);
+}
+
+TEST(test_types, remove_const_and_volatile){
+    bool t1 = same_t<int, sql::remove_cv<int>::type>::value;
+    bool t2 = same_t<int, sql::remove_cv<const int>::type>::value;
+    bool t3 = same_t<int, sql::remove_cv<volatile int>::type>::value;
+    bool t4 = same_t<int, sql::remove_cv<const volatile int>::type>::value;
+    EXPECT_TRUE(t1);
+    EXPECT_TRUE(t2);
+    EXPECT_TRUE(t3);
+    EXPECT_TRUE(t4);
+}
+
+TEST(test_types, conditional_choose_types){
+    using type1 = sql::conditional<true, int, double>::type;
+    using type2 = sql::conditional<false, int, double>::type;
+    bool t1 = same_t<type1, int>::value;
+    bool t2 = same_t<type2, double>::value;
+    EXPECT_TRUE(t1);
+    EXPECT_TRUE(t2);
+}
+
+TEST(test_types, remove_reference){
+    using type1 = sql::remove_reference<int>::type;
+    using type2 = sql::remove_reference<int&>::type;
+    using type3 = sql::remove_reference<int&&>::type;
+    bool t1 = same_t<type1, int>::value;
+    bool t2 = same_t<type2, int>::value;
+    bool t3 = same_t<type3, int>::value;
+    EXPECT_TRUE(t1);
+    EXPECT_TRUE(t2);
+    EXPECT_TRUE(t3);
+}
+
+TEST(test_types, remove_array){
+    using type1 = sql::remove_array<int>::type;
+    using type2 = sql::remove_array<int[]>::type;
+    using type3 = sql::remove_array<int[3]>::type;
+    bool t1 = same_t<type1, int>::value;
+    bool t2 = same_t<type2, int>::value;
+    bool t3 = same_t<type3, int>::value;
+    EXPECT_TRUE(t1);
+    EXPECT_TRUE(t2);
+    EXPECT_TRUE(t3);
+}
+
+TEST(test_types, is_array){
+    bool t1 = sql::is_array<int>::value;
+    bool t2 = sql::is_array<int[]>::value;
+    bool t3 = sql::is_array<int[3]>::value;
+    EXPECT_FALSE(t1);
+    EXPECT_TRUE(t2);
+    EXPECT_TRUE(t3);
+}
+
+TEST(test_types, is_function){
+    bool t1 = sql::is_function<int>::value;
+    bool t2 = sql::is_function<int(int)>::value;
+    EXPECT_FALSE(t1);
+    EXPECT_TRUE(t2);
+}
+
+TEST(test_types, decay_types){
+    using type1 = sql::decay<int&>::type;
+    using type2 = sql::decay<int&&>::type;
+    using type3 = sql::decay<const int>::type;
+    using type4 = sql::decay<int[]>::type;
+    using type5 = sql::decay<int(int)>::type;
+    bool t1 = same_t<type1, int>::value;
+    bool t2 = same_t<type2, int>::value;
+    bool t3 = same_t<type3, int>::value;
+    bool t4 = same_t<type4, int*>::value;
+    bool t5 = same_t<type5, int(*)(int)>::value;
+    EXPECT_TRUE(t1);
+    EXPECT_TRUE(t2);
+    EXPECT_TRUE(t3);
+    EXPECT_TRUE(t4);
+    EXPECT_TRUE(t5);
 }
