6f6497d1f6d10e3e476bddfde6a709e57e2f0ef0
liangti <liangti@foxmail.com>
2020-12-29 15:26:48 -0500

    Add vector

---
+++ include/container/vector.h
@@ -0,0 +1,133 @@
+#ifndef INCLUDED_VECTOR_H
+#define INCLUDED_VECTOR_H
+
+#include <initializer_list>
+
+namespace sql{
+
+template<typename T>
+class Vector{
+public:
+    // default constructor
+    Vector(){
+        _init_space();
+    }
+
+    // initializer list
+    Vector(std::initializer_list<T> l){
+        _init_space();
+        for(auto itr = l.begin(); itr != l.end(); itr++){
+            push_back(*itr);
+        }
+    }
+
+    // shallow copy
+    Vector(const Vector<T>& v){
+        _init_space();
+        for(auto itr = v.cbegin(); itr != v.cend(); itr++){
+            push_back(*itr);
+        }
+    }
+
+    // construct by iterator
+    Vector(const T* vbegin, const T* vend){
+        _init_space();
+        if(vend <= vbegin){
+            return;
+        }
+        for(auto itr = vbegin; itr != vend; itr++){
+            push_back(*itr);
+        }
+    }
+
+    inline T* begin() const{
+        return _begin;
+    }
+
+    inline T* end() const{
+        return _begin + _top;
+    }
+
+    inline const T* cbegin() const{
+        return _begin;
+    }
+
+    inline const T* cend() const{
+        return _begin + _top;
+    }
+
+    bool empty(){
+        return _top == 0;
+    }
+
+    void push_back(const T& t){
+        check_size(_top + 1);
+        T* current = _begin + _top;
+        *current = t;
+        _top++;
+    }
+
+    void pop_back(){
+        if(empty()){
+            return;
+        }
+        _top--;
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args){
+        check_size(_top + 1);
+        // placement new is to create an object in existing address space
+        T* current = new(_begin + _top) T(std::forward<Args>(args)...);
+        _top++;
+    }
+
+    inline size_t size(){
+        return _top;
+    }
+
+    inline T& operator[](size_t idx){
+        return *(_begin + idx);
+    }
+
+    void resize(size_t new_capacity){
+        T* new_begin = _allocate(new_capacity);
+
+        // copy old elements
+        for(size_t i = 0; i < _top; i++){
+            T* current = new_begin + i;
+            T* previous = _begin + i;
+            *current = *previous;
+        }
+        // delete old stuffs
+        delete[] _begin;
+        _begin = new_begin;
+    }
+
+    void check_size(size_t required){
+        if(required > _capacity){
+            _capacity *= 2;
+            resize(_capacity);
+        }
+    }
+
+protected:
+    size_t _capacity;
+    size_t _top;
+    T* _begin;
+    T* _end;
+    void _init_space(size_t capacity=50){
+        _capacity = capacity;
+        _top = 0;
+        _begin = _allocate(capacity);
+        _end = _begin + _capacity;
+    }
+
+    T* _allocate(size_t capacity){
+        return reinterpret_cast<T*>(operator new[](capacity * sizeof(T)));
+    }
+};
+
+}; // using namespace sql
+
+#endif
---
+++ t/07-vector.t.cpp
@@ -0,0 +1,133 @@
+#include <gtest/gtest.h>
+#include <container/vector.h>
+#include <metaprogramming/types.h>
+
+using namespace sql;
+
+class A{
+public:
+    A(int x): data(new int(x)){}
+    A(const A&) = delete;
+    int* data;
+};
+
+TEST(test_vector, simple_create_access){
+    Vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    ASSERT_EQ(v[0], 1);
+    ASSERT_EQ(v[1], 2);
+    ASSERT_EQ(v[2], 3);
+    ASSERT_EQ(v.size(), 3);
+}
+
+TEST(test_vector, resize_when_exceed_capacity){
+    Vector<int> v;
+    size_t test_size = 2000;
+    for(size_t i = 0; i < test_size; i++){
+        v.push_back(i);
+    }
+    ASSERT_EQ(v.size(), test_size);
+    for(size_t i = 0; i < test_size; i++){
+        ASSERT_EQ(v[i], i);
+    }
+}
+
+TEST(test_vector, initialize_by_initializer_list){
+    Vector<int> v = {1, 2, 3};
+    ASSERT_EQ(v[0], 1);
+    ASSERT_EQ(v[1], 2);
+    ASSERT_EQ(v[2], 3);
+    ASSERT_EQ(v.size(), 3);
+
+}
+
+TEST(test_vector, vector_iterator){
+    Vector<int> v = {1, 2, 3};
+    size_t element = 1;
+    for(auto itr = v.begin(); itr != v.end(); itr++){
+        ASSERT_EQ(*itr, element++);
+    }
+}
+
+TEST(test_vector, initialize_by_shallow_copy){
+    Vector<int> v = {1, 2, 3};
+    Vector<int> v2(v);
+    size_t element = 1;
+    for(auto itr = v.begin(); itr != v.end(); itr++){
+        ASSERT_EQ(*itr, element++);
+    }
+}
+
+TEST(test_vector, pop_back){
+    Vector<int> v = {1, 2, 3};
+    v.pop_back();
+    ASSERT_EQ(v.size(), 2);
+    v.push_back(100);
+    ASSERT_EQ(v.size(), 3);
+    ASSERT_EQ(v[0], 1);
+    ASSERT_EQ(v[1], 2);
+    ASSERT_EQ(v[2], 100);
+}
+
+TEST(test_vector, initialize_by_iterator){
+    Vector<int> v = {1, 2, 3};
+    Vector<int> v2(v.cbegin(), v.cend());
+    size_t element = 1;
+    for(auto itr = v.cbegin(); itr != v.cend(); itr++){
+        ASSERT_EQ(*itr, element++);
+    }
+}
+
+TEST(test_vector, pointer_element){
+    Vector<int*> v;
+    int* i = new int(1);
+    v.push_back(i);
+    ASSERT_EQ(*(v[0]), 1);
+}
+
+// it is hard to support reference type
+// since reference require actual value at initialization
+// for example: int& a; is invalid
+// vector may not have initialize at the very beginning
+
+// TEST(test_vector, reference_element){
+//     Vector<sql::reference_wrapper<int>> v;
+//     int i = 3;
+//     v.push_back(i);
+//     ASSERT_EQ(v[0], 1);
+// }
+
+TEST(test_vector, emplace_back_compatible_with_normal_item){
+    Vector<int> v;
+    v.emplace_back(1);
+    ASSERT_EQ(v.size(), 1);
+    ASSERT_EQ(v[0], 1);
+}
+
+TEST(test_vector, initialize_with_no_default_constructor_object){
+    Vector<A> v;
+    size_t test_size = 2000;
+    for(int i = 0; i < test_size; i++){
+        A temp(i);
+        v.push_back(temp);
+    }
+    ASSERT_EQ(v.size(), test_size);
+    for(int i = 0; i < test_size; i++){
+        ASSERT_EQ(*(v[i].data), i);
+    }
+}
+
+TEST(test_vector, emplace_back_does_no_copy){
+    Vector<A> v;
+    size_t test_size = 2000;
+    for(int i = 0; i < test_size; i++){
+        v.emplace_back(i);
+    }
+    ASSERT_EQ(v.size(), test_size);
+    for(int i = 0; i < test_size; i++){
+        ASSERT_EQ(*(v[i].data), i);
+    }
+}
+
