9d8457d0661750627e9c5b66d48984265e32a5f6
liangti <liangti@foxmail.com>
2022-05-29 17:01:56 -0400

    Implement destroy

---
+++ include/memory/destroy.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDED_MEMORY_DESTROY_H
+#define INCLUDED_MEMORY_DESTROY_H
+
+#include <memory/block.h>
+
+namespace sql{
+
+template<class T>
+void destroy_at(T* ptr){
+    ptr->~T();
+}
+
+template<class T>
+void destroy_n(T* ptr, size_t n){
+    for (size_t itr = 0; itr < n; itr++){
+        destroy_at(ptr + itr);
+    }
+}
+
+};
+
+#endif
---
+++ t/destroy.t.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <memory/destroy.h>
+
+using namespace sql;
+
+int reference_count = 0;
+class TestObj{
+public:
+    TestObj(){ reference_count++; }
+    ~TestObj(){ reference_count--; }
+};
+
+TEST(destroy, destroy_at){
+    TestObj *t = new TestObj();
+    ASSERT_EQ(reference_count, 1);
+    destroy_at(t);
+    ASSERT_EQ(reference_count, 0);
+}
+
+TEST(destroy, destroy_n){
+    TestObj t[100];
+    ASSERT_EQ(reference_count, 100);
+    destroy_n(&t[0], 100);
+    ASSERT_EQ(reference_count, 0);
+}
