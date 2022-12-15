994aa8a5361b824c37b4ba87fbf48a6d7dcdac0d
liangti <liangti@foxmail.com>
2022-12-15 13:32:36 -0500

    Thread wrapper memory leak mitigation

--- include/thread/thread.h
+++ include/thread/thread.h
@@ -3,6 +3,7 @@
 
 #include <pthread.h>
 
+#include <memory/allocator.h>
 #include <metaprogramming/tuple.h>
 
 namespace sqrl {
@@ -18,21 +19,31 @@ public:
 
     struct Helper {
       static void *executor(void *args) {
+        // local allocator
+        static sqrl::Allocator<char> allocator;
         fp_pack_t *fp_pack = static_cast<fp_pack_t *>(args);
         sqrl::apply(*fp_pack);
-
+        // clean function pack after executing it
+        allocator.deallocate((char *)args, sizeof(fp_pack));
         // ignore return value
         return NULL;
       }
     }; // internal Helper
 
-    // TODO: memory leak
-    fp_pack_t *fp_pack;
-    fp_pack = new fp_pack_t(std::forward<Callable>(fp),
-                            std::forward<Args...>(args)...);
-    // TODO error handling
+    // memory leak mitigation
+    sqrl::Allocator<char> allocator;
+    size_t pack_size = sizeof(fp_pack_t);
+    char *pack_block = allocator.allocate(pack_size);
+    fp_pack_t *memory_block = reinterpret_cast<fp_pack_t *>(pack_block);
+    fp_pack_t *fp_pack = new (memory_block)
+        fp_pack_t(std::forward<Callable>(fp), std::forward<Args...>(args)...);
+
+    // error handling
     [[maybe_unused]] int succeed = pthread_create(
         &worker, NULL, Helper::executor, reinterpret_cast<void *>(fp_pack));
+    if (succeed != 0) {
+      allocator.deallocate(pack_block, pack_size);
+    }
   }
 
   inline void join() { pthread_join(worker, NULL); }
