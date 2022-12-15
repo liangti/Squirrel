f70f6ef892eac894d77e27b81e85d2eddcce4ce3
liangti <liangti@foxmail.com>
2022-12-15 12:55:30 -0500

    Add type erasure thread wrapper

--- include/thread/thread.h
+++ include/thread/thread.h
@@ -3,17 +3,38 @@
 
 #include <pthread.h>
 
+#include <metaprogramming/tuple.h>
+
 namespace sqrl {
 
 class Thread {
 private:
-  typedef void *(*Fp)(void *);
   pthread_t worker;
 
 public:
-  Thread(Fp fp, void *arg) : worker() {
-    [[maybe_unused]] int succeed = pthread_create(&worker, NULL, fp, arg);
+  template <typename Callable, typename... Args>
+  explicit Thread(Callable &&fp, Args &&...args) : worker() {
+    typedef sqrl::Tuple<Callable, Args...> fp_pack_t;
+
+    struct Helper {
+      static void *executor(void *args) {
+        fp_pack_t *fp_pack = static_cast<fp_pack_t *>(args);
+        sqrl::apply(*fp_pack);
+
+        // ignore return value
+        return NULL;
+      }
+    }; // internal Helper
+
+    // TODO: memory leak
+    fp_pack_t *fp_pack;
+    fp_pack = new fp_pack_t(std::forward<Callable>(fp),
+                            std::forward<Args...>(args)...);
+    // TODO error handling
+    [[maybe_unused]] int succeed = pthread_create(
+        &worker, NULL, Helper::executor, reinterpret_cast<void *>(fp_pack));
   }
+
   inline void join() { pthread_join(worker, NULL); }
 };
 
