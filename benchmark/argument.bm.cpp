5622074bb23bacf5fffdab34d5853dccf4fca016
liangti <liangti@foxmail.com>
2022-12-28 21:30:45 -0500

    Fix function argument benchmark

--- benchmark/argument.bm.cpp
+++ benchmark/argument.bm.cpp
@@ -1,6 +1,6 @@
 #include <benchmark/benchmark.h>
 
-#include <metaprogramming/tuple.h>
+#include <string/string.h>
 
 #ifdef __GNUC__
 #define CALL_CONV __attribute__((cdecl))
@@ -10,63 +10,45 @@
 #warning "Not GCC compiler, will ignore call convention macro"
 #endif
 
-template<typename T>
-auto CALL_CONV value(T t){
-  return t;
-}
+template <typename T> auto CALL_CONV value(T t) { return t; }
 
-template<typename T>
-auto CALL_CONV ref(const T& t){
-  return t;
-}
+template <typename T> auto CALL_CONV ref(const T &t) { return t; }
 
-static void PassByValuePOD1(benchmark::State& state){
-  auto param = sqrl::make_repeat_integer_tuple<1>();
-  for (auto _ : state){
+struct PodObj {
+  char c;
+};
+
+static void PassByValuePOD(benchmark::State &state) {
+  auto param = PodObj();
+  for (auto _ : state) {
     value(param);
   }
 }
 
-static void PassByRefPOD1(benchmark::State& state){
-  auto param = sqrl::make_repeat_integer_tuple<1>();
-  for (auto _ : state){
+static void PassByRefPOD(benchmark::State &state) {
+  auto param = PodObj();
+  for (auto _ : state) {
     ref(param);
   }
 }
 
-static void PassByValuePOD2(benchmark::State& state){
-  auto param = sqrl::make_repeat_integer_tuple<2>();
-  for (auto _ : state){
+static void PassByValueNotPOD(benchmark::State &state) {
+  auto param = sqrl::String("Hello");
+  for (auto _ : state) {
     value(param);
   }
 }
 
-static void PassByRefPOD2(benchmark::State& state){
-  auto param = sqrl::make_repeat_integer_tuple<2>();
-  for (auto _ : state){
+static void PassByRefNotPOD(benchmark::State &state) {
+  auto param = sqrl::String("Hello");
+  for (auto _ : state) {
     ref(param);
   }
 }
 
-static void PassByValuePOD3(benchmark::State& state){
-  auto param = sqrl::make_repeat_integer_tuple<3>();
-  for (auto _ : state){
-    value(param);
-  }
-}
-
-static void PassByRefPOD3(benchmark::State& state){
-  auto param = sqrl::make_repeat_integer_tuple<3>();
-  for (auto _ : state){
-    ref(param);
-  }
-}
-
-BENCHMARK(PassByValuePOD1);
-BENCHMARK(PassByRefPOD1);
-BENCHMARK(PassByValuePOD2);
-BENCHMARK(PassByRefPOD2);
-BENCHMARK(PassByValuePOD3);
-BENCHMARK(PassByRefPOD3);
+BENCHMARK(PassByValuePOD);
+BENCHMARK(PassByRefPOD);
+BENCHMARK(PassByValueNotPOD);
+BENCHMARK(PassByRefNotPOD);
 
 BENCHMARK_MAIN();
