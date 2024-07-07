3820321616e0a8335300d61107becec27ccb866a
liangti <liangti@foxmail.com>
2024-07-07 13:25:33 +0800

    Cleanup AllocatorInterface for C++17

--- include/memory/allocator.h
+++ include/memory/allocator.h
@@ -1,7 +1,9 @@
 #ifndef INCLUDED_MEMORY_ALLOCATOR_H
 #define INCLUDED_MEMORY_ALLOCATOR_H
 #include <concepts>
+
 #include <memory/_allocator_impl.h>
+#include <metaprogramming/types.h>
 
 namespace sqrl {
 
@@ -15,16 +17,19 @@ private:
 public:
   AllocatorInterface() noexcept : impl(){};
   [[nodiscard("Memory leak")]] T *allocate(size_t size) {
-    return (T *)impl.allocate(size * sizeof(T));
+    return impl.allocate(size);
   }
   void deallocate(T *t, size_t size) {
     // size is unused, for compatible with std API
-    impl.deallocate((word_t *)t);
+    impl.deallocate(t);
   }
   ~AllocatorInterface() noexcept = default;
 };
 
 template <typename T> using Allocator = ObjectAllocator<T>;
+
+// compile time check Allocator implementation satisfy AllocatorInterface
+// TODO
 
 #elif __cplusplus >= 202002L
 
